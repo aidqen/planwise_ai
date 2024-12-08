9cd2c327b51820b7c5419bd62d07d97d32158e9c
aidqen <idanmarkin8@gmail.com>
2024-12-09 01:13:09 +0200

    Add title and description inputs

--- src/components/AddNewTask/AddNewTask.jsx
+++ src/components/AddNewTask/AddNewTask.jsx
@@ -6,6 +6,29 @@ export function AddNewTask({}) {
       className="modal shadow-lg bg-black/60"
       open={taskModalOpen}
     >
+      <div className="modal-box flex flex-col h-[90%] w-[90%]">
+        <button className="btn rounded-full min-w-10 max-w-10 min-h-10 max-h-10 p-0 mb-4">
+          <X className="text-white" size={17} onClick={onToggleTaskModal}></X>
+        </button>
+        <div className="flex flex-col gap-4 w-full">
+          <h3 className="font-bold text-lg">Create Task</h3>
+          <input
+            type="text"
+            name="title"
+            onChange={handleChange}
+            placeholder="Title"
+            className="input input-bordered focus:outline-0 w-full max-w-full"
+          />
+          <input
+            type="text"
+            name="description"
+            onChange={handleChange}
+            placeholder="Description"
+            className="input input-bordered focus:outline-0 w-full max-w-full"
+          />
+          <div className="w-full flex flex-row justify-between items-center rounded-[0.5em] input input-bordered">
+        </div>
+      </div>
     </dialog>
   )
 }
